e91e8dcea0d8996484b18384dd8e7eacbb777864
AsaiEri <natsumeyujincho616@gmail.com>
2025-06-14 23:40:49 +0000

    IPアドレス

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Cooking
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << "b0b6d0d15c114d82ac3c463919ecf3f1.vfs.cloud9.ap-northeast-1.amazonaws.com"
+    config.hosts << "18.180.203.162"
   end
 end
